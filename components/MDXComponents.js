be300e68d34df9c987981768acbde30ccaa68885
amreshtech <amreshtech@gmail.com>
2021-04-23 12:33:36 +0530

    clean up

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -3,10 +3,7 @@ import Image from 'next/image';
 
 import ProsCard from '@/components/ProsCard';
 import ConsCard from '@/components/ConsCard';
-import Gumroad from '@/components/metrics/Gumroad';
-import Unsplash from '@/components/metrics/Unsplash';
 import Analytics from '@/components/metrics/Analytics';
-import YouTube from '@/components/metrics/Youtube';
 import Step from '@/components/Step';
 
 const CustomLink = (props) => {
@@ -29,11 +26,8 @@ const MDXComponents = {
   a: CustomLink,
   Analytics,
   ConsCard,
-  Gumroad,
   ProsCard,
-  Step,
-  Unsplash,
-  YouTube
+  Step
 };
 
 export default MDXComponents;
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,12 @@
 import Link from 'next/link';
-import { google } from 'googleapis';
 
-import googleAuth from '@/lib/google/auth';
 import Timeline from '../components/Timeline';
 import Container from '../components/Container';
 import BlogPost from '../components/BlogPost';
 import Subscribe from '../components/Subscribe';
-import ProjectCard from '../components/ProjectCard';
 import VideoCard from '../components/VideoCard';
 
-export async function getStaticProps() {
-  const auth = await googleAuth.getClient();
-  const youtube = google.youtube({
-    auth,
-    version: 'v3'
-  });
-
-  const response = await youtube.videos.list({
-    id: 'Pd2tVxhFnO4,FytxaSVQROc,u_o09PD_qAs',
-    part: 'snippet,statistics'
-  });
-
-  return {
-    props: {
-      videos: response.data.items
-    },
-    revalidate: 60 * 60 // 1 hour
-  };
-}
-
-export default function Home({ videos }) {
+export default function Home() {
   return (
     <Container>
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
@@ -65,24 +42,6 @@ export default function Home({ videos }) {
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
           Courses
         </h3>
-        <ProjectCard
-          title="React 2025"
-          description="Build and deploy a modern Jamstack application using the most popular open-source software."
-          href="https://react2025.com/"
-          icon="react2025"
-        />
-        <ProjectCard
-          title="Learn Next.js"
-          description="A free video course for building static and server-side rendered applications with Next.js and React."
-          href="https://masteringnextjs.com/"
-          icon="nextjs"
-        />
-        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-12 text-black dark:text-white">
-          Recent Videos
-        </h3>
-        {videos.map((video) => (
-          <VideoCard key={video.id} {...video} />
-        ))}
         <Timeline />
         <Subscribe />
       </div>
