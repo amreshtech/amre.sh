1096d0484e337de6bdee00d71c61544a23b803b3
Amresh Mishra <ammishra@overstock.com>
2021-10-13 19:27:18 +0100

    allow vercel vital analytics

--- next.config.js
+++ next.config.js
@@ -47,7 +47,7 @@ module.exports = {
 // https://securityheaders.com
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com *.vercel-insights.com;
   child-src *.youtube.com *.google.com *.twitter.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com *.gstatic.com;
   img-src * blob: data:;
