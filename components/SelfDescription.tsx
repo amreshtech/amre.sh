34a17766b60dac36db2163f506c18fe5bf74893b
Amresh Mishra <ammishra@overstock.com>
2022-02-15 22:03:07 +0000

    added self description-responsive home

---
+++ components/SelfDescription.tsx
@@ -0,0 +1,15 @@
+const SelfDescription = () => (
+  <section className="text-black dark:text-gray-400 text-md pb-12 dark:bg-black w-full md:w-2/3">
+    <p className="pb-2">
+      I am a Front-End Developer based in Ireland. I write about technology,
+      finance, investments and my thoughts about the shape that tech industry
+      will take in our lifetime. I hope you will love to come along with me on
+      my journey.
+    </p>
+    <p>
+      I also document my journey across the world through amazing photos which
+      you can view or even buy as NFTs!
+    </p>
+  </section>
+);
+export default SelfDescription;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import Tweet from '@components/widgets/Tweet';
 import { getMap } from '@lib/map';
 import Chat from '@components/widgets/Chat';
 import Image from 'next/image';
+import SelfDescription from '@components/SelfDescription';
 interface Props {
   map_url: string;
   tweetData: TweetType;
@@ -21,11 +22,11 @@ const Blog: React.FC<Props> = ({ map_url, tweetData }) => {
       <div className="flex flex-col justify-center items-start max-w-3xl mx-auto mb-16 z-10 w-full">
         <div className="flex flex-row w-full gap-4">
           <div className="w-2/3">
-            <h1 className="font-bold text-3xl pt-24 md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+            <h1 className="font-bold text-2xl pt-24 md:text-5xl tracking-tight mb-4 text-black dark:text-white">
               Hey, I&apos;m Amresh 👋
             </h1>
             <div className="relative w-full mb-4">
-              <h1 className="flex gap-2 pb-24 font-bold text-3xl md:text-5xl tracking-tight text-black dark:text-white">
+              <h1 className="flex gap-2 pb-12 font-bold text-2xl md:text-5xl tracking-tight text-black dark:text-white">
                 I&apos;m
                 <Typewriter
                   options={{ loop: true }}
@@ -63,7 +64,8 @@ const Blog: React.FC<Props> = ({ map_url, tweetData }) => {
             />
           </div>
         </div>
-        <div className="relative w-full mb-4 flex flex-row gap-2">
+        <SelfDescription />
+        <div className="relative w-full mb-4 flex-row gap-2 hidden md:flex">
           <div className="w-1/4">
             <Map url={map_url} />
           </div>
@@ -77,6 +79,24 @@ const Blog: React.FC<Props> = ({ map_url, tweetData }) => {
           </div>
           <div className="w-1/4">
             <Chat />
+          </div>
+        </div>
+        <div className="relative w-full mb-4 flex flex-col gap-2 md:hidden">
+          <div className="w-full">
+            <Tweet
+              id={tweetData.id}
+              created_at={tweetData.created_at}
+              text={tweetData.text}
+              public_metrics={tweetData.public_metrics}
+            />
+          </div>
+          <div className="flex flex-row gap-2">
+            <div className="w-1/2">
+              <Map url={map_url} />
+            </div>
+            <div className="w-1/2">
+              <Chat />
+            </div>
           </div>
         </div>
       </div>
