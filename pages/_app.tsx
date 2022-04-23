add11b1a276c1b7c4dbaf9a6641e68b1e08e51ef
Amresh Mishra <amreshtech@gmail.com>
2022-04-23 09:01:28 +0100

    try cloudflare analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,23 +10,9 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </SimpleReactLightbox>
       <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script
-        id="ga-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-              cookie_flags: 'SameSite=None;Secure',
-            });
-          `
-        }}
+        defer={true}
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon={`{"token": "${process.env.NEXT_PUBLIC_CLOUDFLARE_ANALYTICS}"}`}
       />
     </ThemeProvider>
   );
