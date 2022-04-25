f5308bfb41dd6f18e7d5f34a37662269c9ac2950
Amresh Mishra <amreshtech@gmail.com>
2022-04-25 12:20:21 +0100

    fixed rerender issues

--- components/Container.tsx
+++ components/Container.tsx
@@ -6,6 +6,7 @@ import Footer from '@components/Footer';
 import Particles from 'react-tsparticles';
 import particlesJson from '../particles.json';
 import { NextSeo, SocialProfileJsonLd } from 'next-seo';
+import React from 'react';
 
 export default function Container(props) {
   const [mounted, setMounted] = useState(false);
@@ -23,6 +24,43 @@ export default function Container(props) {
     type: 'website',
     ...customMeta
   };
+  const navLinks = [
+    { label: 'Blog', link: '/blog' },
+    { label: 'Travel', link: '/travel' },
+    { label: 'About', link: '/about' }
+  ];
+
+  const ThemeButton = () => (
+    <button
+      aria-label="Toggle Dark Mode"
+      type="button"
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        stroke="currentColor"
+        className="h-4 w-4 text-gray-800 dark:text-gray-200"
+      >
+        {resolvedTheme === 'dark' ? (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+          />
+        ) : (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        )}
+      </svg>
+    </button>
+  );
 
   return (
     <div className="bg-white dark:bg-black">
@@ -74,70 +112,23 @@ export default function Container(props) {
         <div>
           {router.pathname !== '/' && (
             <NextLink href="/">
-              <a>
-                <div className="font-qc text-4xl text-black dark:text-white font-bold hidden sm:block">
-                  amresh mishra
-                </div>
-                <div className="font-qc text-4xl text-black dark:text-white font-bold block sm:hidden">
-                  amresh
-                </div>
+              <a className="flex flex-row font-qc text-4xl text-black dark:text-white font-bold gap-2">
+                <span>amresh</span>
+                <span className="hidden sm:block">mishra</span>
               </a>
             </NextLink>
           )}
         </div>
         <div className="flex flex-row items-center">
-          {!hideNav && (
-            <>
-              <NextLink href="/blog">
+          {!hideNav &&
+            navLinks.map(({ label, link }) => (
+              <NextLink href={link} key={link}>
                 <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100">
-                  Blog
+                  {label}
                 </a>
               </NextLink>
-              <NextLink href="/travel">
-                <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100">
-                  Travel
-                </a>
-              </NextLink>
-              <NextLink href="/about">
-                <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100 mr-2">
-                  About
-                </a>
-              </NextLink>
-            </>
-          )}
-          <button
-            aria-label="Toggle Dark Mode"
-            type="button"
-            onClick={() =>
-              setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-            }
-          >
-            {mounted && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                stroke="currentColor"
-                className="h-4 w-4 text-gray-800 dark:text-gray-200"
-              >
-                {resolvedTheme === 'dark' ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                  />
-                )}
-              </svg>
-            )}
-          </button>
+            ))}
+          <ThemeButton />
         </div>
       </nav>
       <main
@@ -145,8 +136,8 @@ export default function Container(props) {
         className="flex flex-col justify-center bg-white dark:bg-black px-4"
       >
         {children}
-        <Footer />
       </main>
+      <Footer />
     </div>
   );
 }
