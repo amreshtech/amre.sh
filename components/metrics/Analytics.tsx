b5b35f3886576b785a1e1d7b2b5f79e5a984b498
Amresh Mishra <amreshtech@gmail.com>
2022-05-01 11:07:02 +0100

    improve SEO

--- components/metrics/Analytics.tsx
+++ components/metrics/Analytics.tsx
@@ -8,7 +8,7 @@ export default function Analytics() {
   const { data } = useSWR('/api/views', fetcher);
 
   const pageViews = format(data?.total);
-  const link = 'https://amre.sh';
+  const link = 'https://www.amre.sh/';
 
   return <MetricCard header="All-Time Views" link={link} metric={pageViews} />;
 }
