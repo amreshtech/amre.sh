c85266e916a42b0bb678a6b07b88d5cc19be0917
Amresh Mishra <amreshtech@gmail.com>
2022-12-03 14:02:31 +0000

    Fix search (#96)
    * moved from contentful to contentlayer
    
    * fix algolia search
    
    * cleanup

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -10,39 +10,24 @@ interface Props {
   summary: string;
   slug: string;
   tags: string[];
-  _highlightResult?: { title: any; summary: any; slug: any };
   image?: string;
 }
 
-const BlogPost: React.FC<Props> = ({
-  title,
-  summary,
-  slug,
-  tags,
-  _highlightResult
-}) => {
+const BlogPost: React.FC<Props> = ({ title, summary, slug, tags }) => {
   const { data } = useSWR(`/api/views/${slug}`, fetcher);
   const views = data?.total;
   return (
     <Link href={slug} className="w-full">
       <div className="mb-8 w-full">
         <div className="flex flex-col md:flex-row justify-between">
-          <h4
-            className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100"
-            dangerouslySetInnerHTML={{
-              __html: _highlightResult?.title?.value || title
-            }}
-          ></h4>
+          <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
+            {title}
+          </h4>
           <p className="text-gray-500 text-left md:text-right w-32 mb-4 md:mb-0">
             {!!views && <ViewCounter slug={slug} />}
           </p>
         </div>
-        <p
-          className="text-gray-600 dark:text-gray-400"
-          dangerouslySetInnerHTML={{
-            __html: _highlightResult?.summary?.value || summary
-          }}
-        ></p>
+        <p className="text-gray-600 dark:text-gray-400">{summary}</p>
         <div className="flex flex-row items-center gap-2 pt-2">
           {tags?.map((tag, index) => (
             <Badge text={tag} key={tag} index={index} />
