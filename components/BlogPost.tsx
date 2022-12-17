d2b5388c5d38fafa0a19a774f829431a90638c94
Amresh Mishra <amreshtech@gmail.com>
2022-12-17 19:04:02 +0000

    remove unreliable view counter (#108)
    * remove unreliable view counter
    
    * fixes

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -1,9 +1,6 @@
 import Link from 'next/link';
-import useSWR from 'swr';
-import fetcher from '@lib/fetcher';
 import * as React from 'react';
 import Badge from './Badge';
-import ViewCounter from './ViewCounter';
 
 interface Props {
   title: string;
@@ -14,8 +11,6 @@ interface Props {
 }
 
 const BlogPost: React.FC<Props> = ({ title, summary, slug, tags }) => {
-  const { data } = useSWR(`/api/views/${slug}`, fetcher);
-  const views = data?.total;
   return (
     <Link href={slug} className="w-full">
       <div className="mb-8 w-full">
@@ -23,9 +18,6 @@ const BlogPost: React.FC<Props> = ({ title, summary, slug, tags }) => {
           <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
             {title}
           </h4>
-          <p className="text-gray-500 text-left md:text-right w-32 mb-4 md:mb-0">
-            {!!views && <ViewCounter slug={slug} />}
-          </p>
         </div>
         <p className="text-gray-600 dark:text-gray-400">{summary}</p>
         <div className="flex flex-row items-center gap-2 pt-2">
