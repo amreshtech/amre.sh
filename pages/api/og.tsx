42ee371466303e6011331d1caae0f0c48b19061a
amreshtech <amreshtech@gmail.com>
2023-01-04 22:00:52 +0000

    remove references to missing font

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,20 +1,11 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
 
-export const config = {
-  runtime: 'experimental-edge'
-};
-
-const quicksand = fetch(
-  new URL('../../styles/quicksand.ttf', import.meta.url)
-).then((res) => res.arrayBuffer());
-
 export default async function handler(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const title = decodeURIComponent(searchParams.get('title'));
   const backgroundImage = searchParams.get('image');
   const imageUrl = `https://images.unsplash.com/photo-${backgroundImage}`;
-  const quicksandData = await quicksand;
 
   return new ImageResponse(
     (
@@ -71,8 +62,7 @@ export default async function handler(req: NextRequest) {
             width: '60%',
             display: 'flex',
             left: 0,
-            top: '130px',
-            fontFamily: '"Quicksand"'
+            top: '130px'
           }}
         >
           {title}
@@ -81,15 +71,7 @@ export default async function handler(req: NextRequest) {
     ),
     {
       width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: 'Quicksand',
-          data: quicksandData,
-          style: 'normal',
-          weight: 900
-        }
-      ]
+      height: 630
     }
   );
 }
