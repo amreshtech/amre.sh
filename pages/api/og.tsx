5c27a92c49cab68203855966b8ff80a5ec6bedfe
Amresh Mishra <amreshtech@gmail.com>
2023-01-09 20:27:00 +0000

    fixed og image (#121)

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,6 +1,9 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
 
+export const config = {
+  runtime: 'experimental-edge'
+};
 export default async function handler(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const title = decodeURIComponent(searchParams.get('title'));
